0d1aecbf357e57b3f14501dd7e8b6c531da5a7c0
Michael Wakahe <mikewakahe@gmail.com>
2022-03-20 21:19:06 +0300

    Setting global username/password in Postgres restore script

--- PostgreSQL/PostgreSQL Tutorial/DVD_Rental_Dataset/restore.sh
+++ PostgreSQL/PostgreSQL Tutorial/DVD_Rental_Dataset/restore.sh
@@ -3,17 +3,22 @@
 set -eux
 set -o pipefail
 
+# The superuser username and password of the database
+root_user='postgres'
+root_pass='postgres'
+
+# The credentials for the sample database
 db_user='foo'
 db_pass='baz'
 db_name='sampledb'
 
 # Create the database user and database
-PGPASSWORD=example psql --username=postgres --no-password --host=localhost -c "DROP DATABASE IF EXISTS $db_name;"
-PGPASSWORD=example psql --username=postgres --no-password --host=localhost -c "DROP USER IF EXISTS $db_user;"
+PGPASSWORD=$root_pass psql --username=$root_user --no-password --host=localhost -c "DROP DATABASE IF EXISTS $db_name;"
+PGPASSWORD=$root_pass psql --username=$root_user --no-password --host=localhost -c "DROP USER IF EXISTS $db_user;"
 
-PGPASSWORD=example psql --username=postgres --no-password --host=localhost -c "CREATE DATABASE $db_name;"
-PGPASSWORD=example psql --username=postgres --no-password --host=localhost -c "CREATE USER $db_user WITH ENCRYPTED PASSWORD '$db_pass';"
-PGPASSWORD=example psql --username=postgres --no-password --host=localhost -c "GRANT ALL PRIVILEGES ON DATABASE $db_name TO $db_user;"
+PGPASSWORD=$root_pass psql --username=$root_user --no-password --host=localhost -c "CREATE DATABASE $db_name;"
+PGPASSWORD=$root_pass psql --username=$root_user --no-password --host=localhost -c "CREATE USER $db_user WITH ENCRYPTED PASSWORD '$db_pass';"
+PGPASSWORD=$root_pass psql --username=$root_user --no-password --host=localhost -c "GRANT ALL PRIVILEGES ON DATABASE $db_name TO $db_user;"
 
 # Restore a dump of the database
 PGPASSWORD=$db_pass psql --host=localhost --dbname=$db_name --username=$db_user -w < $db_name.pgdump
